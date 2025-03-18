e884910fd39da069e6a509bac5e66c4fa06576fd
Jorge Olivares <jorenr.72@hotmail.com>
2025-03-19 00:22:43 +0100

    bTransactionAsAService: add dependencies and parameters to make Transactions as a Service

--- src/main/java/com/service/AccountService/AccountServiceApplication.java
+++ src/main/java/com/service/AccountService/AccountServiceApplication.java
@@ -2,16 +2,10 @@ package com.service.AccountService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.web.reactive.HttpHandlerAutoConfiguration.AnnotationConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-import com.service.AccountService.repositories.CustomersRepo;
-
-@SpringBootApplication()
+@SpringBootApplication
+@EnableEurekaServer
 public class AccountServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountServiceApplication.class, args);
